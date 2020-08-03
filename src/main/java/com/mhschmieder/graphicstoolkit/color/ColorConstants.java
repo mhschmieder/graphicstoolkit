f72558e7b85b6c255d7327e3241e068b3f7e12d5
Mark Schmieder <markhschmieder@gmail.com>
2020-08-02 18:57:26 -0700

    Update ColorConstants.java
    Added a lot of useful defined colors for the common monochrome grays and for the remaining set of sixteen named colors for the ones that aren't already named in AWT. A few additional useful hues were added as well, that are also not in the CSS3 color spec but should be.

--- src/main/java/com/mhschmieder/graphicstoolkit/color/ColorConstants.java
+++ src/main/java/com/mhschmieder/graphicstoolkit/color/ColorConstants.java
@@ -31,6 +31,8 @@
  */
 package com.mhschmieder.graphicstoolkit.color;
 
+import java.awt.Color;
+
 /**
  * {@code ColorConstants} is a container for constants related to color
  * handling, such as lookup indices for individual color components of HSB.
@@ -128,5 +130,184 @@ public final class ColorConstants {
      * The number of specified individual CMYK components, which is invariant.
      */
     public static final int                             NUMBER_OF_CMYK_COMPONENTS = 4;
+
+    //////// Custom colors to augment the AWT named color constants //////////
+
+    /**
+     * Maroon is is one of the sixteen named colors, but isn't present in AWT.
+     */
+    public static final Color                           MAROON                    =
+                                                               new Color( 128, 0, 0 );
+
+    /**
+     * Olive is is one of the sixteen named colors, but isn't present in AWT.
+     */
+    public static final Color                           OLIVE                     =
+                                                              new Color( 128, 128, 0 );
+
+    /**
+     * Lime is is one of the sixteen named colors, but isn't present in AWT.
+     */
+    public static final Color                           LIME                      =
+                                                             new Color( 0, 128, 0 );
+
+    /**
+     * Teal is is one of the sixteen named colors, but isn't present in AWT.
+     */
+    public static final Color                           TEAL                      =
+                                                             new Color( 0, 128, 128 );
+
+    /**
+     * Cyan is is one of the sixteen named colors, but isn't present in AWT.
+     * <p>
+     * This color is alternately known as aqua, but cyan is more common.
+     */
+    public static final Color                           CYAN                      =
+                                                             new Color( 0, 255, 255 );
+
+    /**
+     * Navy is is one of the sixteen named colors, but isn't present in AWT.
+     */
+    public static final Color                           NAVY                      =
+                                                             new Color( 0, 0, 128 );
+
+    /**
+     * Purple is is one of the sixteen named colors, but isn't present in AWT.
+     */
+    public static final Color                           PURPLE                    =
+                                                               new Color( 128, 0, 128 );
+
+    /**
+     * Magenta is is one of the sixteen named colors, but isn't present in AWT.
+     * <p>
+     * This color is alternately known as fuschia, but magenta is more common.
+     */
+    public static final Color                           MAGENTA                   =
+                                                                new Color( 255, 0, 255 );
+
+    /**
+     * Bright Yellow is defined in many specifications, but not AWT or CSS3.
+     */
+    public static final Color                           BRIGHTYELLOW              =
+                                                                     new Color( 255, 255, 10 );
+
+    /**
+     * Dark Royal Blue is defined in many specifications, but not AWT or CSS3.
+     */
+    public static final Color                           DARKROYALBLUE             =
+                                                                      new Color( 62, 86, 151 );
+
+    /**
+     * Lemon is defined in many specifications, but not AWT or CSS3.
+     */
+    public static final Color                           LEMON                     =
+                                                              new Color( 202, 202, 70 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 10% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY10                    =
+                                                               new Color( 26, 26, 26 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 20% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY20                    =
+                                                               new Color( 51, 51, 51 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 25% Gray is
+     * common as it represents the simple 1/4 ratio.
+     */
+    public static final Color                           GRAY25                    =
+                                                               new Color( 64, 64, 64 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 30% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY30                    =
+                                                               new Color( 77, 77, 77 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 33.3% Gray is
+     * common as it represents the simple 1/3 ratio.
+     */
+    public static final Color                           GRAY33_3                  =
+                                                                 new Color( 85, 85, 85 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 40% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY40                    =
+                                                               new Color( 102, 102, 102 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 45% Gray is a
+     * safe bet when 50% Gray might cause masking of foreground vs. background.
+     */
+    public static final Color                           GRAY45                    =
+                                                               new Color( 115, 115, 115 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 50% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY50                    =
+                                                               new Color( 128, 128, 128 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 55% Gray is a
+     * safe bet when 50% Gray might cause masking of foreground vs. background.
+     */
+    public static final Color                           GRAY55                    =
+                                                               new Color( 140, 140, 140 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 60% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY60                    =
+                                                               new Color( 153, 153, 153 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 66.6% Gray is
+     * common as it represents the simple 2/3 ratio.
+     */
+    public static final Color                           GRAY66_6                  =
+                                                                 new Color( 170, 170, 170 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 70% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY70                    =
+                                                               new Color( 177, 177, 177 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 75% Gray is
+     * common as it represents the simple 3/4 ratio.
+     * <p>
+     * Additionally, this monochrome ratio is commonly known as silver in CSS3.
+     */
+    public static final Color                           GRAY75                    =
+                                                               new Color( 192, 192, 192 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 80% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY80                    =
+                                                               new Color( 203, 203, 203 );
+
+    /**
+     * Many monochromatic grays are needed by most applications; 90% Gray is one
+     * of the eight most-used ten-percentile divisions between Black and White.
+     */
+    public static final Color                           GRAY90                    =
+                                                               new Color( 231, 231, 231 );
 
 }
